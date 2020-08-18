aaa0de3d1079bbe003f0b31b6a7147a24128cd1a
Neytorokx <31908898+Neytoro@users.noreply.github.com>
2020-08-18 12:10:17 -0500

    Binary Search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,14 @@
 def linear_search(arr, target):
     # Your code here
 
+    index = 0;
+
+    if len(arr) > 0:
+        for i in arr:
+            if i == target:
+                return index;
+            else:
+                index += 1;
 
     return -1   # not found
 
@@ -8,7 +16,19 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    mid = 0;
+    left = 0;
+    right = len(arr) - 1;
+  
+    while left <= right: 
+  
+        mid = (right + left) // 2;
 
-
-    return -1  # not found
+        if arr[mid] < target: 
+            left = mid + 1;
+        elif arr[mid] > target: 
+            right = mid - 1;
+        else: 
+            return mid;
+  
+    return -1; # not found
